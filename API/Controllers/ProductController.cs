2cd1208aae46753eeb8982a57bd1df5785cf0653
Issacboox <nurarat0024@gmail.com>
2023-11-17 01:53:55 +0700

    setup

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -29,6 +29,16 @@ namespace API.Controllers
             var product = await _repo.GetProductByIdAsync(id);
             return Ok(product);
         }
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
 
     }
 }
