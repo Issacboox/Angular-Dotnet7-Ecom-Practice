3b3e7c5c521ebb8a3801a461744c17e119118a3b
Issacboox <nurarat0024@gmail.com>
2023-11-17 00:30:59 +0700

    Backend Setup

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,29 +11,23 @@ namespace API.Controllers
     public class ProductController : Controller
     {
         private readonly StoreContext _context;
-        public ProductController(StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
-
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return products;
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<List<Product>>> GetProducts(int id)
         {
-            var product = await _context.Products.FindAsync(id);
-
-            if (product == null)
-            {
-                return NotFound(); // Or another appropriate IActionResult for not found
-            }
-
-            return new List<Product> { product };
+            var product = await _repo.GetProductByIdAsync(id);
+            return Ok(product);
         }
 
     }
